1c9742beff5e2d74ff451568efece4aac3d0370d
Katy DeCorah <katydecorah@gmail.com>
2020-06-19 15:46:50 -0400

    :wrench: update dependencies and better error handling

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Turn a Spotify playlist into a Jekyll post.
 
 1. Create a [Spotify app](https://developer.spotify.com/dashboard/applications).
 2. From terminal, export the following variables using the information from your Spotify app:
+
 ```
 echo "export SpotifyClientID=0000ffff0000ffff0000ffff0000ffff0000ffff" >> ~/.bash_profile
 echo "export SpotifyClientSecret=0000ffff0000ffff0000ffff0000ffff0000ffff" >> ~/.bash_profile
-echo "export SpotifyUser=0000ffff0000" >> ~/.bash_profile
 ```
 
 Install the app:
@@ -30,6 +30,6 @@ You can find your playlist's ID in its URL.
 
 There are a few assumptions made about how playlists are named in Spotify and the Jekyll structure.
 
-* Spotify playlists are named `YYYY season`. For example: `2018 Summer` and `2018/2019 Winter`. You can [manipulate the ingested playlist name in the `formatTracks` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L44-L47).
-* Jekyll posts are in `playlists/_posts/`. You can [change that in the `createPost` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L69).
-* Images are in `img/playlists/` directories. You can [change that in the `buildPost` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L84) and [`saveImage` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L123).
+- Spotify playlists are named `YYYY season`. For example: `2018 Summer` and `2018/2019 Winter`. You can [manipulate the ingested playlist name in the `formatTracks` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L44-L47).
+- Jekyll posts are in `playlists/_posts/`. You can [change that in the `createPost` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L69).
+- Images are in `img/playlists/` directories. You can [change that in the `buildPost` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L84) and [`saveImage` function](https://github.com/katydecorah/spotify-to-jekyll/blob/fc88b4eff599074ebae58fa3dd8e574761edb050/index.js#L123).
--- index.js
+++ index.js
@@ -1,76 +1,84 @@
-const _ = require('underscore');
-const fs = require('fs');
-const moment = require('moment');
-const Jimp = require('jimp');
-const SpotifyWebApi = require('spotify-web-api-node');
+const _ = require("underscore");
+const fs = require("fs");
+const moment = require("moment");
+const Jimp = require("jimp");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 module.exports.playlist = (event, context, callback) => {
+  if (!process.env.SpotifyClientID)
+    throw new Error(
+      "Missing `SpotifyClientID` environment variable please see README.md"
+    );
+  if (!process.env.SpotifyClientSecret)
+    throw new Error(
+      "Missing `SpotifyClientSecret` environment variable please see README.md"
+    );
   module.exports
     .getPlaylist()
     .then(module.exports.formatTracks)
     // create new post
-    .then(data => module.exports.createPost(data))
+    .then((data) => module.exports.createPost(data))
     // save tracks to playlists.yml
-    .then(data => module.exports.updateMaster(data))
+    .then((data) => module.exports.updateMaster(data))
     // save image to img/playlists/
-    .then(data => module.exports.saveImage(data))
-    .then(data => callback(null, data))
-    .catch(err => callback(err));
+    .then((data) => module.exports.saveImage(data))
+    .then((data) => callback(null, data))
+    .catch((err) => callback(err));
 };
 
 module.exports.getPlaylist = () => {
   const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SpotifyClientID,
-    clientSecret: process.env.SpotifyClientSecret
+    clientSecret: process.env.SpotifyClientSecret,
   });
 
   return spotifyApi
     .clientCredentialsGrant()
-    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-    .then(() =>
-      spotifyApi.getPlaylist(
-        process.env.SpotifyUser,
-        process.env.SpotifyPlaylist
-      )
-    )
-    .then(data => data.body)
-    .catch(err => err);
+    .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+    .then(() => spotifyApi.getPlaylist(process.env.SpotifyPlaylist))
+    .then((data) => data.body)
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
 
-module.exports.formatTracks = data => {
-  return new Promise(resolve => {
+module.exports.formatTracks = (data) => {
+  return new Promise((resolve) => {
     resolve({
       name: data.name,
       formatted_name: data.name
-        .replace('/', '-')
+        .replace("/", "-")
         .toLowerCase()
-        .replace(' ', '-'),
-      url: data.external_urls.spotify,
+        .replace(" ", "-"),
+      url:
+        data.external_urls && data.external_urls.spotify
+          ? data.external_urls.spotify
+          : "",
       tracks: module.exports.getTracks(data.tracks),
-      image: _.findWhere(data.images, { width: 640 }).url
+      image: _.findWhere(data.images, { width: 640 }).url,
     });
   });
 };
 
-module.exports.getTracks = tracks => {
+module.exports.getTracks = (tracks) => {
   return tracks.items.reduce((arr, item) => {
     arr.push({
       name: item.track.name,
-      artist: _.pluck(item.track.artists, 'name').join(', '),
-      album: item.track.album.name
+      artist: _.pluck(item.track.artists, "name").join(", "),
+      album: item.track.album.name,
     });
     return arr;
   }, []);
 };
 
-module.exports.createPost = data => {
+module.exports.createPost = (data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(
-      `playlists/_posts/${moment().format('YYYY-MM-DD')}-${
+      `playlists/_posts/${moment().format("YYYY-MM-DD")}-${
         data.formatted_name
       }.md`,
       module.exports.buildPost(data),
-      err => {
+      (err) => {
         if (err) return reject(err);
         resolve(data);
       }
@@ -78,24 +86,20 @@ module.exports.createPost = data => {
   });
 };
 
-module.exports.buildPost = data => {
-  let contents = `---\ntitle: ${data.name}\nspotify: ${
-    data.url
-  }\nimage: img/playlists/${data.formatted_name}.png\npermalink: /playlists/${
-    data.formatted_name
-  }/\n---\n\n[Listen on Spotify](${data.url})\n\n`;
-  data.tracks.map(track => {
+module.exports.buildPost = (data) => {
+  let contents = `---\ntitle: ${data.name}\nspotify: ${data.url}\nimage: img/playlists/${data.formatted_name}.png\npermalink: /playlists/${data.formatted_name}/\n---\n\n[Listen on Spotify](${data.url})\n\n`;
+  data.tracks.map((track) => {
     contents += `* ${track.name}, ${track.artist}\n`;
   });
   return contents;
 };
 
-module.exports.updateMaster = data => {
+module.exports.updateMaster = (data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(
-      '_data/playlists.yml',
+      "_data/playlists.yml",
       module.exports.buildNewMaster(data),
-      err => {
+      (err) => {
         if (err) return reject(err);
         resolve(data);
       }
@@ -103,10 +107,10 @@ module.exports.updateMaster = data => {
   });
 };
 
-module.exports.buildNewMaster = data => {
-  let content = fs.readFileSync('_data/playlists.yml').toString('utf8');
+module.exports.buildNewMaster = (data) => {
+  let content = fs.readFileSync("_data/playlists.yml").toString("utf8");
   content += `- playlist: ${data.name}\n  spotify: ${data.url}\n  tracks:\n`;
-  data.tracks.map(track => {
+  data.tracks.map((track) => {
     content += `  - track: ${JSON.stringify(
       track.name
     )}\n    artist: ${JSON.stringify(
@@ -116,12 +120,12 @@ module.exports.buildNewMaster = data => {
   return content;
 };
 
-module.exports.saveImage = data => {
+module.exports.saveImage = (data) => {
   return new Promise((resolve, reject) => {
     Jimp.read(data.image, (err, img) => {
       if (err) return reject(err);
       img.rgba(false).write(`img/playlists/${data.formatted_name}.png`);
-      resolve('done!');
+      resolve("done!");
     });
   });
 };
